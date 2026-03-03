46243cfe9c45fcd7988532e53003c1a1018d5046
Amruth Gowda <amruthgowda5002@gmail.com>
2026-03-03 09:16:59 +0530

    Add Merge_two_sorted_array.py

---
+++ Array/merge_two_sorted_array.py
@@ -0,0 +1,27 @@
+class Solution:
+    def merge(self,nums1,nums2):
+        i=0
+        j=0
+        result=[]
+        while i<len(nums1) and j<len(nums2):
+            if nums1[i]<=nums2[j]:
+                if len(result)==0 or result[-1]!=nums1[i]:
+                    result.append(nums1[i])
+                i+=1
+            else:
+                if len(result)==0 or result[-1]!=nums2[j]:
+                    result.append(nums2[j])
+                j+=1
+        while i<len(nums1):
+            if len(result)==0 or result[-1]!=nums1[i]:
+                result.append(nums1[i])
+            i+=1
+        while j<len(nums2):
+            if len(result)==0 or result[-1]!=nums2[j]:
+                result.append(nums2[j])
+            j+=1
+        return result
+s1=Solution()
+nums1=[1,1,2,3,4,6,7]
+nums2=[1,2,5,6,6,8,9,10]
+print(s1.merge(nums1,nums2))
