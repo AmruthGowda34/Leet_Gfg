9131f223ca8d988bedad1714f35abf7ec183e03c
Amruth Gowda <amruthgowda5002@gmail.com>
2026-04-17 19:13:16 +0530

    Add find_duplicate.py

---
+++ Array/find_duplicate.py
@@ -0,0 +1,21 @@
+class Solution:
+    def fun(self,nums):
+        slow=nums[0]
+        fast=nums[0]
+        
+        while True:
+            slow=nums[slow]
+            fast=nums[nums[fast]]
+            if slow==fast:
+                break
+        
+        slow=nums[0]
+        while slow!=fast:
+            slow=nums[slow]
+            fast=nums[fast]
+        
+        return slow
+
+s1= Solution()
+nums = [3,1,3,4,2]
+print(s1.fun(nums))
