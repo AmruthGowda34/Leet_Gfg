aeb948bb2040caebb84b3e80563e45bef4d00650
Amruth Gowda <amruthgowda5002@gmail.com>
2026-05-11 21:29:55 +0530

    Add min_stack.py

---
+++ Stack/min_stack.py
@@ -0,0 +1,31 @@
+class Solution:
+    def __init__(self):
+        self.items=[]
+    
+    def push(self,val):
+        if len(self.items)==0:
+            self.items.append([val,val])
+        else:
+            mini=min(self.items[-1][1],val)
+            self.items.append([val,mini])
+    
+    def pop(self):
+        return self.items.pop()
+    
+    def get_min(self):
+        if len(self.items)==0:
+            return "It is empty"
+        return self.items[-1][1]
+    
+    def top(self):
+        if len(self.items)==0:
+            return "It is empty"
+        return self.items[-1][0]
+
+s1=Solution()
+s1.push(3)
+s1.push(4)
+s1.push(5)
+print(s1.get_min())
+print(s1.pop())
+print(s1.get_min())
