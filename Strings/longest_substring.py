3249cfd8d1bef0b4fa056dbd78a64d939c6c60ab
Amruth Gowda <amruthgowda5002@gmail.com>
2026-06-13 23:19:18 +0530

    update longest_substring.py

--- Strings/longest_substring.py
+++ Strings/longest_substring.py
@@ -1,14 +1,29 @@
 def longest_sub_string(str):
-    seen=set()
+    
+    #------------Bruteforce-------------------------
+    # max_len=float('-inf')
+    # for i in range(len(str)):
+    #     my_set=set()
+    #     for j in range(i,len(str)):
+    #         if str[j] in my_set:
+    #             break
+    #         max_len=max(max_len,j-i+1)
+    #         my_set.add(str[j])
+    # return max_len
+
+
+    #----------Optimal-------------------------
+    max_len=float('-inf')
     l=0
-    max_length=0
+    seen=set()
     for r in range(len(str)):
         while str[r] in seen:
             seen.remove(str[l])
             l+=1
+        max_len=max(max_len,r-l+1)
         seen.add(str[r])
-        max_length=max(max_length,r-l+1)
-    return max_length
+    return max_len
     
-print(longest_sub_string("abccbacbccad"))
+        
 print(longest_sub_string("abcdabccbafg"))
+print(longest_sub_string("CADBZABCDE"))
